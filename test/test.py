98f2c8acc6267e02cc2586b53e37ef92d2c38a54
Franco-Barto <franco.bartesaghi@correo.ucu.edu.uy>
2024-10-31 14:42:29 -0300

    Check proyect 7

--- test/test.py
+++ test/test.py
@@ -118,6 +118,7 @@ async def test_project(dut):
     
     dut._log.info("Test project 7 behaviour")
     for i in range(9):        
+        dut._log.info("Check at "+str(i))
         dut.ui_in.value = 112
         await ClockCycles(dut.clk, 1)
         assert dut.uo_out.value == display_7seg_an(i%8)
